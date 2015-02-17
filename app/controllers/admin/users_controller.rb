d4e182c791c0acfd4fad861f4edebe3bc8d0573e
Nicolanice <nicolanice8362@gmail.com>
2015-02-17 17:36:54 +0300

    users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,53 +1,36 @@
 #encoding: utf-8
 
 class Admin::UsersController < ApplicationController
-  before_filter :authenticate_user!
-  before_filter :set_user, only: [:edit, :update, :destroy]
+  respond_to :html
 
   load_and_authorize_resource
 
   def index
-    authorize! :read, User
   end
 
   def new
-    authorize! :create, User
-    @user = User.new
   end
 
   def edit
   end
 
   def create
-    authorize! :create, User
     @user = User.new(user_params)
-    if @user.save
-      redirect_to admin_users_path, notice: "Пользователь зарегистрирован"
-    else
-      render :new
-    end
+    flash[:notice] = "Пользователь зарегистрирован" if @user.save
+    respond_with(:admin, @user)
   end
 
   def update
-    authorize! :update, @user
-    if @user.update_attributes(user_params)
-      redirect_to admin_users_path, notice: "Данные пользователя изменены"
-    else
-      render :new
-    end
+    flash[:notice] = "Данные пользователя изменены" if @user.update(user_params)
+    respond_with(:admin, @user)
   end
 
   def destroy
-    authorize! :destroy, @user
-    @user.destroy
-    redirect_to admin_users_path, notice: "Пользователь удален"
+    flash[:notice] = "Пользователь удален" if @user.destroy
+    respond_with(:admin, @user)
   end
 
   private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:login, :nickname, :email, :password, :password_confirmation)
